a4ff74d0c07f543d23675b68447a4a3294b9fde2
Lucas Fonseca <lucasfonseca@lara.unb.br>
2018-10-31 08:38:01 -0300

    Python 3

--- graph.py
+++ graph.py
@@ -1,13 +1,17 @@
 import serial
 import time
 import binascii
+import struct
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
 
 number_of_sensors = 1
-p = True
+p = False
 time_to_measure = 10
-command = 0
+command = 1
 
-addresses = [2,8,7,3,4,5,1]
+addresses = [2,8,2,3,4,5,1]
 
 portIMU = '/dev/tty.usbmodem14101' # rPi
 serial_port = serial.Serial(port=portIMU, baudrate=115200, timeout=0.001)
@@ -48,19 +52,30 @@ def read_sensors(portIMU):
             break
 
         bytes_to_read = serial_port.inWaiting()
-        if bytes_to_read > 0:
-            data = serial_port.read(bytes_to_read)
-            id_str = binascii.hexlify(data[1].encode('utf-8'))
-            if p:
-                print(id_str)
 
-            try:
-                id = int(id_str)
-                for i in range(len(addresses)):
-                    if id == addresses[i]:
-                        counters[i] += 1
-            except ValueError:
-                print('NAN')
+        if bytes_to_read > 0:
+            data = bytearray(serial_port.read(bytes_to_read)).decode()
+
+            # angle
+            b = ''.join(chr(i) for i in data[8:12])  # angle x
+            ang = struct.unpack('>f', b)
+            x = ang[0]
+            # print(x)
+
+            # angle
+            b = ''.join(chr(i) for i in data[12:16])  # angle y
+            ang = struct.unpack('>f', b)
+            y = ang[0]
+
+            # angle
+            b = ''.join(chr(i) for i in data[16:20])  # angle z
+            ang = struct.unpack('>f', b)
+            z = ang[0]
+
+            print(x,y,z)
+            counters[0] += 1
+        else:
+            print("No data")
 
     for i in range(len(addresses)):
         serial_port.write(('>'+str(addresses[i])+',86\n').encode())
